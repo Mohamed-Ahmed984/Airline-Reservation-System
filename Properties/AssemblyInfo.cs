ccfad7cff4663344818533d045d5b42407a1bb0d
Mohamed Ahmed <125696152+Mohamed-Ahmed984@users.noreply.github.com>
2026-09-20 09:27:23 +0300

    chore: add assembly metadata

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Airline Management App")]
+[assembly: AssemblyDescription("Windows Forms application for managing airline flights and passengers")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Airline Management App")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: ComVisible(false)]
+[assembly: Guid("491a770f-4546-4f62-9968-92644d73bdde")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
